ebbc7a7867870460af3b580846366e7c38ee32db
Diego Osuna <7rgsx7d6jz@privaterelay.appleid.com>
2026-03-21 01:00:23 +0000

    got to work on the routes

--- src/api/models.py
+++ src/api/models.py
@@ -1,15 +1,23 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import String, Boolean
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import String, Boolean, ForeignKey, Column, Table
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from typing import List
 
 db = SQLAlchemy()
 
+
 class User(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True)
-    email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
+    email: Mapped[str] = mapped_column(
+        String(120), unique=True, nullable=False)
     password: Mapped[str] = mapped_column(nullable=False)
     is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False)
-
+    favorite_characters: Mapped[List["Character"]] = relationship(
+        secondary = "favorite_character",
+        back_populates="favorited_by")
+    favorite_planets: Mapped[List["Planet"]] = relationship(
+        secondary = "favorite_planet",
+        back_populates="favorited_by")
 
     def serialize(self):
         return {
@@ -17,3 +25,54 @@ class User(db.Model):
             "email": self.email,
             # do not serialize the password, its a security breach
         }
+
+favorite_character = Table(
+     "favorite_character",
+     db.metadata,
+     Column("user_id", ForeignKey("user.id")),
+     Column("character_id", ForeignKey("character.id"))   
+)    
+
+favorite_planet = Table(
+     "favorite_planet",
+     db.metadata,
+     Column("user_id", ForeignKey("user.id")),
+     Column("planet_id", ForeignKey("planet.id"))  
+)
+
+class Character(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(120))
+    age: Mapped[str] = mapped_column(String(120))
+    planet_of_origin: Mapped[int] = mapped_column(ForeignKey("planet.id"))
+    favorited_by: Mapped[List["User"]] = relationship(
+        secondary = "favorite_character",
+        back_populates="favorite_characters")
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name":self.name,
+            "age": self.age,
+            "planet_of_origin": self.planet_of_origin.serialize
+            # do not serialize the password, its a security breach
+        }
+
+class Planet(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    density: Mapped[str] = mapped_column(String(120))
+    population: Mapped[str] = mapped_column(String(120))
+    favorited_by: Mapped[List["User"]] = relationship(
+        secondary ="favorite_planet",
+        back_populates="favorite_planets")
+    
+    def serialize(self):
+         return {
+            "id": self.id,
+            "density": self.density,
+            "population": 
+        }
+        
+        
+
+
--- src/api/routes.py
+++ src/api/routes.py
@@ -2,9 +2,10 @@
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
 from flask import Flask, request, jsonify, url_for, Blueprint
-from api.models import db, User
+from api.models import db, User, Character, Planet
 from api.utils import generate_sitemap, APIException
 from flask_cors import CORS
+from sqlalchemy import select
 
 api = Blueprint('api', __name__)
 
@@ -20,3 +21,29 @@ def handle_hello():
     }
 
     return jsonify(response_body), 200
+
+@api.route('/people', methods=['GET'])
+def get_all_characters():
+    all_people = db.session.execute(select(Character)).scalars().all()
+    list_of_dictionaries = []
+    for person in all_people:
+        list_of_dictionaries.apend(person.serialize())
+    
+    return jsonify(list_of_dictionaries), 200
+
+@api.route('/people/<int:person_id>', methods=["GET"])
+def get_single_person(person_id):
+    person = db.session.get(Character, person_id) 
+    if person is None:
+        return jsonify({"msg": "charcater does not exist"}), 404
+    return jsonify(person.serialize()), 200 #with fetches and responses back end and front end
+   
+
+@api.route('/planets', methods=["GET"])
+def get_all_planets(planets_id):
+    pass
+
+@api.route('/planets/<int:planet_id>', methods=["GET"])
+def get_single_planet(planet_id):
+    pass
+
